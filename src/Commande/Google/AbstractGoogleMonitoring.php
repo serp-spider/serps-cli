c14fb51ff604d65bb8464fa52da4060029a323ac
sghzal <sghzal@gmail.com>
2016-04-21 10:31:07 +0200

    fix monitoring

--- src/Commande/Google/AbstractGoogleMonitoring.php
+++ src/Commande/Google/AbstractGoogleMonitoring.php
@@ -18,7 +18,7 @@ abstract class AbstractGoogleMonitoring extends Command
 
     public function execute(){
         $googleClient = new GoogleClient($this->getHttpClient());
-        $googleClient->setUserAgent('Mozilla/5.0 (Windows NT 6.1) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/41.0.2228.0 Safari/537.36');
+        $googleClient->request->setUserAgent('Mozilla/5.0 (Windows NT 6.1) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/41.0.2228.0 Safari/537.36');
 
         $url = new GoogleUrl("google.fr");
         $url->setSearchTerm("simpsons the movie trailer");
